5ef20a2ffa6bd19ed9dbbea1b685aace5d0c1f8f
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-04-23 14:40:16 +0300

    Make the function "get_console_arguments"

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,33 @@
+import argparse
+import os
+import pathlib
+
+
+def get_console_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', help='enter a directory path')
+    args = parser.parse_args()
+    return args
+
+
+def get_dublicates(path):
+    # Path.samefile(otherpath)
+    # p = Path('.')
+    # [x for x in p.iterdir() if x.is_dir()]
+    pass
+
+
+def print_dublicates(files):
+    pass
+
 
 if __name__ == '__main__':
-    pass
+    console_arguments = get_console_arguments()
+    directory_path = console_arguments.directory
+    print(directory_path)
+    directory_existing = os.path.exists(directory_path)
+    if directory_existing:
+        dublicates = get_dublicates(directory_path)
+    if not directory_existing:
+        exit('Can not find the directory.')
+    print_dublicates(dublicates)

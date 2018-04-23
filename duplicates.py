a0f1c5e5c1f5807fd51b1c92037d89e5ff524e2f
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-04-23 21:15:41 +0300

    Output a list of all files.

--- duplicates.py
+++ duplicates.py
@@ -10,11 +10,22 @@ def get_console_arguments():
     return args
 
 
+def get_all_files_in_directory(path, file_paths):
+    for file_system_object in path.iterdir():
+        if file_system_object.is_dir():
+            file_paths = get_all_files_in_directory(
+                file_system_object,
+                file_paths
+            )
+        if file_system_object.is_file():
+            file_paths.append(file_system_object)
+    return file_paths
+
+
 def get_dublicates(path):
-    # Path.samefile(otherpath)
-    # p = Path('.')
-    # [x for x in p.iterdir() if x.is_dir()]
-    pass
+    all_file_paths = []
+    all_file_paths = get_all_files_in_directory(path, all_file_paths)
+    print(all_file_paths)
 
 
 def print_dublicates(files):
@@ -23,11 +34,12 @@ def print_dublicates(files):
 
 if __name__ == '__main__':
     console_arguments = get_console_arguments()
-    directory_path = console_arguments.directory
-    print(directory_path)
-    directory_existing = os.path.exists(directory_path)
-    if directory_existing:
-        dublicates = get_dublicates(directory_path)
+    user_path = console_arguments.directory
+    directory_existing = os.path.exists(user_path)
     if not directory_existing:
         exit('Can not find the directory.')
+    user_path = pathlib.Path(user_path)
+    if not user_path.is_dir():
+        exit('The entered path is not a directory path.')
+    dublicates = get_dublicates(user_path)
     print_dublicates(dublicates)

b3c10b7c15e539e1f0e12d03809b095173711c45
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-04-25 01:50:11 +0300

    Simplify the code having replaced keys and values in file_info dictionary.

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-from collections import Counter
 
 
 def get_console_arguments():
@@ -10,10 +9,12 @@ def get_console_arguments():
     return args
 
 
-def get_file_name_and_size(file_path):
-    file_name = file_path.name
-    file_size = file_path.stat().st_size
-    return file_name, file_size
+def get_file_info_dict(file_features, path, file_dict):
+    if file_features in file_dict:
+        file_dict[file_features].append(path)
+        return file_dict
+    file_dict[file_features] = [path]
+    return file_dict
 
 
 def get_all_files_in_directory(path):
@@ -22,45 +23,22 @@ def get_all_files_in_directory(path):
         for file_name in filenames:
             file_path = os.path.join(path, file_name)
             file_size = os.path.getsize(file_path)
-            file_info_dict[file_path] = (file_name, file_size)
+            file_name_and_size = (file_name, file_size)
+            file_info_dict = get_file_info_dict(
+                file_name_and_size,
+                file_path,
+                file_info_dict
+            )
     return file_info_dict
-
-
-def get_paths_of_duplicate_file(duplicate_name_and_size, all_files_info):
-    duplicate_path_list = [
-        file_path
-        for file_path, file_name_and_size in all_files_info
-        if file_name_and_size == duplicate_name_and_size
-    ]
-    return duplicate_path_list
-
-
-def get_all_duplicate_files(duplicate_items):
-    duplicate_list = [
-        file_name_and_size
-        for file_name_and_size, file_repeats_number in duplicate_items
-        if file_repeats_number > 1
-    ]
-    return duplicate_list
-
-
-def get_name_and_path_of_duplicates(duplicate_counter_items, all_files_items):
-    duplicates_dict = {}
-    duplicate_file_list = get_all_duplicate_files(duplicate_counter_items)
-    for file_name_and_size in duplicate_file_list:
-        duplicate_file_paths = get_paths_of_duplicate_file(file_name_and_size, all_files_items)
-        file_name = file_name_and_size[0]
-        duplicates_dict[file_name] = duplicate_file_paths
-    return duplicates_dict
 
 
 def get_duplicate_file_names_and_paths(path):
     all_files_info = get_all_files_in_directory(path)
-    duplicate_counter = Counter(all_files_info.values())
-    all_duplicates = get_name_and_path_of_duplicates(
-        duplicate_counter.items(),
-        all_files_info.items()
-    )
+    all_duplicates = {}
+    for file_name_and_size, file_path_list in all_files_info.items():
+        if len(file_path_list)>1:
+            file_name = file_name_and_size[0]
+            all_duplicates[file_name] = file_path_list
     return all_duplicates
 
 
@@ -69,11 +47,11 @@ def print_duplicates(duplicate_dict):
         print('The directory does not contain duplicate files.')
     if duplicate_dict:
         print('The directory contains these duplicate files:')
-        for file in duplicate_dict:
-            print(file)
-            all_paths = duplicate_dict[file]
+        for file_name in duplicate_dict:
+            print(file_name)
+            all_paths = duplicate_dict[file_name]
             for path in all_paths:
-                print('       {}'.format(path))
+                print('\t', path)
 
 
 if __name__ == '__main__':
